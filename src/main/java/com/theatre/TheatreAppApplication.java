27750b7cc2cb534abd822aac1b4c07db9ccbfc71
turab45 <turabbajeer202@gmail.com>
2021-11-09 19:35:11 +0500

    add thymleaf

---
+++ src/main/java/com/theatre/TheatreAppApplication.java
@@ -0,0 +1,13 @@
+package com.theatre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheatreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheatreAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/theatre/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.theatre.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("title", "Theatrey - Home");
+		return "index";
+	}
+}
